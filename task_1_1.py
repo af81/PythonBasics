cb7edb515bb560ceae1f692632e553421ece66c8
af81 <af@metapage.ch>
2025-09-07 10:40:46 +0200

    feat: add solution to task 1

--- task_1_1.py
+++ task_1_1.py
@@ -1,5 +1,19 @@
-# Hier kommt Augabe 1 aus Block 1 hin
-# Du kannst den Code in diesem File verändern, um die Aufgabe zu lösen
-# Viel Erfolg!
+# Einfache Ein- und Ausgabe
 
-print("Block 1 - Aufgabe 1")
+# Eingabe des Namens
+print("Wie heisst du?")
+name = input("Gib deinen Namen ein: ")
+
+# Ausgabe einer Begrüssung
+print(f"Hallo {name}! Schön, dass du da bist")  # Alternativ: print("Hallo " + name + "! Schön, dass du da bist")
+
+# Eingabe zweier Zahlen
+print("Lass uns zwei Zahlen addieren.")
+zahl1 = input("Gib die erste Zahl ein: ")
+zahl2 = input("Gib die zweite Zahl ein: ")
+
+# Umwanldung der Eingaben in Zahlen und Berechnung der Summe
+summe = float(zahl1) + float(zahl2)  # Umwandlung
+
+# Ausgabe der Summe
+print(f"Die Summe von {zahl1} und {zahl2} ist {summe}.")
